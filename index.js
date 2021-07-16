d292519e9c0fb5764ed99fdf08b8233a28091533
Allison Homem <mrs.allisonhomem@outlook.com>
2021-07-16 11:29:19 -0500

    finished task 6 in javascript

--- index.js
+++ index.js
@@ -291,11 +291,21 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+const newArtist =  { 
+  id: 20,
+  name: "Allison Homem", 
+  years: "1991 - 16 July 2021",
+  genre: "Web Design", 
+  nationality: "American",
+  bio: "Allison loves fresh pour-overs and pastries. Sometimes you can find her staying up til midnight making pasta."
+};  
 
+function addArtist(array, newobject) {
+  array.push(newobject);
+  return array;
+};
 
+console.log(addArtist(artists, newArtist));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use lotsOfArt to do the following: 
